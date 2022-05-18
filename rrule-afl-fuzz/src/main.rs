5e5417b8d34182821446398bb41fb12f76792a76
Omid Rad <omidmr@gmail.com>
2022-05-18 11:40:54 +0200

    Remove duplicated fuzzes

--- rrule-afl-fuzz/src/main.rs
+++ rrule-afl-fuzz/src/main.rs
@@ -5,14 +5,14 @@ use take_rrule::take_rrule_from_data;
 
 use afl::fuzz;
 use core::str::FromStr;
-use rrule::{DateFilter, RRuleSet, RRuleSet};
+use rrule::{DateFilter, RRuleSet};
 
 #[allow(clippy::single_match)]
 fn main() {
-    let fuzz_selector: u8 = 4;
+    let fuzz_selector: u8 = 2;
     // let fuzz_target
     match fuzz_selector {
-        // Fuzz the RRule from string
+        // Fuzz the RRuleSet from string
         0 => fuzz!(|data: &[u8]| {
             if let Ok(s) = std::str::from_utf8(data) {
                 // Everything here is acceptable, but panic is not what we want.
@@ -27,28 +27,6 @@ fn main() {
             if let Ok(s) = std::str::from_utf8(data) {
                 // Everything here is acceptable, but panic is not what we want.
                 match RRuleSet::from_str(s) {
-                    Ok(_rule) => {}
-                    Err(_) => {}
-                }
-            }
-        }),
-        // Fuzz the RRule from string and list all
-        2 => fuzz!(|data: &[u8]| {
-            if let Ok(s) = std::str::from_utf8(data) {
-                // Everything here is acceptable, but panic is not what we want.
-                match RRuleSet::from_str(s) {
-                    Ok(rule) => {
-                        let _ = rule.into_iter().all(50);
-                    }
-                    Err(_) => {}
-                }
-            }
-        }),
-        // Fuzz the RRuleSet from string
-        3 => fuzz!(|data: &[u8]| {
-            if let Ok(s) = std::str::from_utf8(data) {
-                // Everything here is acceptable, but panic is not what we want.
-                match RRuleSet::from_str(s) {
                     Ok(rule) => {
                         let _ = rule.into_iter().all(50);
                     }
@@ -57,7 +35,7 @@ fn main() {
             }
         }),
         // Fuzz directly to RRule, skip parser
-        4 => fuzz!(|data: &[u8]| {
+        2 => fuzz!(|data: &[u8]| {
             let rule = match take_rrule_from_data(data) {
                 Some(rule) => rule,
                 None => return, // Not enough data to create `RRule`
