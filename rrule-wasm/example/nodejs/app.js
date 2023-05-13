f6b499cdd105796dfa92141ad75eb966b9f96271
Douglas Machado <douglas.machado@valor-software.com>
2023-05-13 07:07:15 -0300

    add nodejs wasm example

---
+++ rrule-wasm/example/nodejs/app.js
@@ -0,0 +1,20 @@
+const { get_all_date_recurrences } = require('../../pkg/rrule_wasm.js');
+
+const http = require('http');
+const url = require('url');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  const queryObject = url.parse(req.url,true).query;
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  var data = get_all_date_recurrences("DTSTART:20120201T093000Z\nRRULE:FREQ=DAILY;COUNT=3", 100);
+  console.log(data);
+  res.end(data.toString());
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
