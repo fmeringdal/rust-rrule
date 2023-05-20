391b3f2c1f3622565ccebf48a8388a7f676dba36
Douglas Machado <douglas.machado@valor-software.com>
2023-05-19 23:46:26 -0300

    code review fixes

--- rrule/src/wasm/mod.rs
+++ rrule/src/wasm/mod.rs
@@ -2,17 +2,19 @@ use wasm_bindgen::prelude::*;
 
 use crate::{RRuleSet, RRuleError};
 
-/**
-  Get all recurrences of the rrule!
-     rule_set_string: List of rrules
-     limit: Limit must be set in order to prevent infinite loops
-*/
+/// Get all recurrences of the rrule
+///
+/// # Arguments
+///
+/// * `rule_set` - List of rrules
+///
+/// * `limit` - Limit must be set in order to prevent infinite loops
 #[wasm_bindgen]
 pub fn get_all_date_recurrences(rule_set: &str, limit: Option<u16>) -> Result<Vec<JsValue>, JsError> {
     let rrule_result: Result<RRuleSet, RRuleError> = rule_set.parse();
     match rrule_result {
         Ok(rrule) =>  {
-            //  Set hard limit in case of infinitely recurring rules
+            ///  Set hard limit in case of infinitely recurring rules
             let rule_set_collection = rrule.all(limit.unwrap_or(100));
             let result: Vec<JsValue> = rule_set_collection.dates
                 .into_iter()
