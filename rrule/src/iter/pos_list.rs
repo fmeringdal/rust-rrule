3cf50a698b20481ea383c190f6bae1d643452e36
Fredrik Meringdal <fmeringdal@hotmail.com>
2024-02-04 17:17:15 +0100

    Fix clippy lints

--- rrule/src/iter/pos_list.rs
+++ rrule/src/iter/pos_list.rs
@@ -17,8 +17,7 @@ pub(crate) fn build_pos_list(
 
     let timeset_len = u32::try_from(timeset.len())
         .expect("timeset length is a maximum of 24 * 60 * 60 which is covered by u32");
-    let timeset_len_float = f64::try_from(timeset_len)
-        .expect("timeset length is a maximum of 24 * 60 * 60 which is covered by f64");
+    let timeset_len_float = f64::from(timeset_len);
     let timeset_len_int = i32::try_from(timeset_len)
         .expect("timeset length is a maximum of 24 * 60 * 60 which is covered by i32");
     for pos in by_set_pos {
